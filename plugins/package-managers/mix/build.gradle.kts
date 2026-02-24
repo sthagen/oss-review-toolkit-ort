oss-review-toolkit/ort

feat(analyzer): Add Mix package manager plugin for Elixir
Add support for analyzing Elixir projects using mix_sbom for CycloneDX
SBOM generation. Supports single projects and umbrella workspaces with
proper handling of Hex packages.

Signed-off-by: Jonatan Männchen <jonatan@maennchen.ch>

plugins/package-managers/mix/build.gradle.kts
@@ -1,0 +1,38 @@
+/*
+ * Copyright (C) 2026 The ORT Project Copyright Holders <https://github.com/oss-review-toolkit/ort/blob/main/NOTICE>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+plugins {
+    // Apply precompiled plugins.
+    id("ort-plugin-conventions")
+}
+
+dependencies {
+    api(projects.analyzer)
+    api(projects.model)
+
+    implementation(projects.downloader)
+    implementation(projects.utils.commonUtils)
+    implementation(projects.utils.cyclonedxUtils)
+
+    implementation(libs.cyclonedx)
+
+    ksp(projects.analyzer)
+
+    funTestImplementation(testFixtures(projects.analyzer))
+}

sthagen/oss-review-toolkit-ort

plugins/package-managers/mix/build.gradle.kts
@@ -1,0 +1,36 @@
+/*
+ * Copyright (C) 2026 The ORT Project Copyright Holders <https://github.com/oss-review-toolkit/ort/blob/main/NOTICE>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * License-Filename: LICENSE
+ */
+
+plugins {
+    // Apply precompiled plugins.
+    id("ort-plugin-conventions")
+}
+
+dependencies {
+    api(projects.analyzer)
+    api(projects.model)
+    api(projects.plugins.api)
+    api(projects.utils.cyclonedxUtils)
+
+    implementation(projects.utils.commonUtils)
+
+    ksp(projects.analyzer)
+
+    funTestImplementation(testFixtures(projects.analyzer))
+}